87380cac4ef51a5a9b5f36fb2dc6ac2a1e81e7ba
Miguel-DevTech <Miguelnogueiraneto@gmail.com>
2025-03-19 21:50:33 -0300

    Fixing navbar style import

--- restaurante-app/src/components/NavBar/NavBar.tsx
+++ restaurante-app/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import "./Navbar.css";
+import "./NavBar.css";
 
 function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
